89376595d55e06a07b32049b9c7fdced3968f5d6
Carmeldj <carmeldjegui01@gmail.com>
2024-04-04 16:32:19 +0100

    Start and fiish the reapeatstring task and run it' test

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
+const repeatString = function (str, nbr) {
+  let result = "";
 
+  if (nbr >= 0) {
+    for (let i = 0; i < nbr; i++) {
+      result = result.concat(str);
+    }
+    return result;
+  } else return "ERROR";
 };
+console.log(repeatString("hey", 3));
 
 // Do not edit below this line
 module.exports = repeatString;
