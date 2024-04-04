ad9b33e750595684c1b46527c50dd7d30c58af95
Carmeldj <carmeldjegui01@gmail.com>
2024-04-04 19:35:14 +0100

    Start and finish the task reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
-
+const reverseString = function (str) {
+  //   let a = str.split(" ");
+  //   let s1 = "";
+  //   let s2 = "";
+  //   for (let i = 0; i < a.length; i++) {
+  //     a[i] = a[i].split("").reverse();
+  //   }
+  //   a = a.reverse();
+  //   for (let i = 0; i < a.length; i++) {
+  //     s1 = a[i].join("");
+  //     s2 = s2 + s1 + " ";
+  //   }
+  //   s2 = s2.slice(0, -1);
+  //   return s2;
+  return str.split("").reverse().join("");
 };
 
+console.log(reverseString("hello world"));
 // Do not edit below this line
 module.exports = reverseString;
