25a5fc144e11b035adbddb3ceaa9d093772e7d78
Carmeldj <carmeldjegui01@gmail.com>
2024-04-05 13:37:07 +0100

    Fix my code to pass all tests
    I didn't pass all the test the first time so I retry with the includes()
    method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,14 @@
 const removeFromArray = function (a, ...args) {
-  // let b = [];
+  let b = [];
   for (i = 0; i < a.length; i++) {
-    for (const arg of args) {
-      if (a[i] == arg) {
-        a.splice(i, 1);
-      }
+    if (!args.includes(a[i])) {
+      b.push(a[i]);
     }
   }
-  console.log(a.length);
-  return a;
+  return b;
 };
 
-console.log(removeFromArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2, 3, 7, 10));
+console.log(removeFromArray([1, 2, 3, 4, 3, 3], 3, 1));
 
 // Do not edit below this line
 module.exports = removeFromArray;
