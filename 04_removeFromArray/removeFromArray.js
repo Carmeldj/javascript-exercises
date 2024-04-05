5da406e655b10aa399cb549ce2d1eec50d25d58c
Carmeldj <carmeldjegui01@gmail.com>
2024-04-05 13:09:39 +0100

    Start and finish the remove from array task

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,17 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (a, ...args) {
+  // let b = [];
+  for (i = 0; i < a.length; i++) {
+    for (const arg of args) {
+      if (a[i] == arg) {
+        a.splice(i, 1);
+      }
+    }
+  }
+  console.log(a.length);
+  return a;
 };
+
+console.log(removeFromArray([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 2, 3, 7, 10));
 
 // Do not edit below this line
 module.exports = removeFromArray;
