8e2c5aca07d8cb1093e908dccfcd20d5975c1fcb
Carmeldj <carmeldjegui01@gmail.com>
2024-04-05 18:28:12 +0100

    Start and finish of temp conversion task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,20 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (degree) {
+  return round((degree - 32) * (5 / 9), 0);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (degree) {
+  return round(degree * (9 / 5) + 32, 1);
 };
 
+function round(value, precision) {
+  var multiplier = Math.pow(10, precision || 1);
+  return Math.round(value * multiplier) / multiplier;
+}
+
+console.log(convertToFahrenheit(0));
+console.log(convertToCelsius(32));
 // Do not edit below this line
 module.exports = {
   convertToCelsius,
-  convertToFahrenheit
+  convertToFahrenheit,
 };
